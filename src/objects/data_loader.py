5640b2ae378944f5e3c2970f35c44940d49545ef
deplanty <yvon.deplante@gmail.com>
2023-08-31 18:47:15 +0200

    WIP Data loader as plugins

--- src/objects/data_loader.py
+++ src/objects/data_loader.py
@@ -34,3 +34,38 @@ class DataLoader:
                     break
                 # Add the 5 columns in data
                 self.data.add_row(line[:5], x_index=0)
+
+
+class LoaderDefault:
+    file_ext: list[str]
+    file_desc: str
+
+    def load(self):
+        ...
+
+    @property
+    def file_type(self):
+        return self.file_desc + " (*." + ", *.".join(self.file_ext) + ")"
+
+
+
+class DataContainerLoader:
+    def __init__(self):
+        import glob
+        import os
+
+        loaders = glob.glob(os.path.join(os.path.dirname(__file__), "loaders", "*.py"))
+        for pyfile in loaders:
+            data = DataContainer()
+            with open(pyfile) as fid:
+                exec(fid.read(), None, dict(filename="test/dummy.csv", data=data))
+
+        subclasses = LoaderDefault.__subclasses__()
+        print(subclasses)
+        subc = list()
+        for c in subclasses:
+            subc.append(c())
+        print(subc[0].file_type)
+
+
+loaders = DataContainerLoader()
